bcfe68aab1f521d852a8e727edd351db44679b2c
Vladimir Torres Mirez <vladitorresmirez@gmail.com>
2018-05-17 20:18:28 -0500

    Avance 1 de proyectos

---
+++ app/Controller/ProyectosController.php
@@ -0,0 +1,210 @@
+<?php
+class ProyectosController extends AppController{
+	public $name = 'Proyecto';	
+	
+	public function index($page=null,$order_by=null,$order_by_or=null,$search_proyecto=null) {
+		/*if($this->obj_logged_user->getAttr('tipo_user_id') == 2) {
+        	$this->redirect(array('controller' => 'errors', 'action' => 'error_404'));
+			exit();
+        }*/
+
+		$this->layout = "default_adm";
+		$this->loadModel('Proyecto');
+		
+		$page = 0;
+		//$page -= 1;
+		$per_page = 10;
+		$start = $page * $per_page;
+		
+		if($order_by_or!=NULL && isset($order_by_or) && $order_by_or!='null'){
+			$order_by_or = $order_by_or;
+		}else{
+			$order_by_or = 'DESC';
+		}
+	
+		$order_by = 'Proyecto.created';
+		
+		if($this->request->is('get')){
+			if($search_proyecto!=''){
+				$search_proyecto = $search_proyecto;
+			}else{
+				$search_proyecto = '';
+			}
+			
+		}else{
+			$search_proyecto = '';
+		}
+		
+		$list_proyecto_all = $this->Proyecto->listAllProyectos($order_by, utf8_encode($search_proyecto),$order_by_or);
+		$list_proyectos = $this->Proyecto->listFindProyectos($order_by, $search_proyecto,$order_by_or, $start, $per_page);
+		$count = count($list_proyecto_all);
+		$no_of_paginations = ceil($count / $per_page);
+		$page = $page + 1;
+		
+		$this->set(compact('list_proyectos','page','no_of_paginations'));
+	}
+	
+	public function find_proyectos($page=null,$order_by=null,$order_by_or=null,$search_proyecto=null) {
+		$this->layout = 'ajax';
+		$this->loadModel('Proyecto');
+		$page = $page;
+		$page -= 1;
+		$per_page = 10;
+		$start = $page * $per_page;
+		/*if(isset($order_by)){
+			$order_by = $order_by;
+		}else{
+			$order_by = 'Persona.created';
+		}*/
+		$order_by = 'Proyecto.created';
+	
+		if($order_by_or!=NULL && isset($order_by_or) && $order_by_or!='null'){
+			$order_by_or = $order_by_or;
+		}else{
+			$order_by_or = 'DESC';
+		}
+	
+		/*if($order_by=='title'){
+			$order_by = 'Bit.title';
+		}elseif($order_by=='username'){
+			$order_by = 'UserJoin.username';
+		}elseif($order_by=='home'){
+			$order_by = 'Bit.view_home';
+		}elseif($order_by=='status'){
+			$order_by = 'Bit.status';
+		}else{
+			$order_by = 'Bit.created';
+		}*/
+	
+		if($this->request->is('get')){
+		
+			if($search_proyecto == 'null'){
+				$search_proyecto = '';
+			}else{
+				$search_proyecto = $search_proyecto;
+			}
+
+		}else{
+			$search_proyecto = '';
+		}
+
+		$list_proyecto_all = $this->Proyecto->listAllProyectos($order_by, utf8_encode($search_proyecto),$order_by_or);
+		$list_proyectos = $this->Proyecto->listFindProyectos($order_by, utf8_encode($search_proyecto),$order_by_or, $start, $per_page);
+		$count = count($list_proyecto_all);
+		$no_of_paginations = ceil($count / $per_page);
+		$page = $page+1;
+		$this->set(compact('list_proyectos','page','no_of_paginations'));
+	}
+	
+	
+	/**
+	 * Add and Edit using Ajax
+	 * 16 March 2015
+	 * @author Vladimir
+	 */
+	public function add_edit_proyecto($proyecto_id=null){
+		$this->layout = 'ajax';
+		
+		if($this->request->is('post')  || $this->request->is('put')){
+			if(isset($proyecto_id) && intval($proyecto_id) > 0){
+				
+				//update
+				
+				$this->Proyecto->id = $proyecto_id;
+
+				if($this->request->data['Proyecto']['imagen']['name'] != ''){
+					
+					$imagen = $this->request->data['Proyecto']['imagen']['name'];
+					$arr = explode(".", $imagen);
+					$extension = strtolower(array_pop($arr));
+					$new_file_name = time().'.'.$extension;
+					
+					$this->request->data['Proyecto']['imagen'] = $new_file_name;
+						
+					//$image_tmp = $this->request->data['Proyecto']['imagen']['tmp_name'];
+					
+					
+					$uploaddir = APP.WEBROOT_DIR.'/files/proyecto/';
+					$uploadfile = $uploaddir . basename($new_file_name);
+				
+					move_uploaded_file($_FILES['data']['tmp_name']['Proyecto']['imagen'], $uploadfile);
+				
+				}else{
+					unset($this->request->data['Proyecto']['imagen']);
+				}
+	
+				if ($this->Proyecto->save($this->request->data)) {
+					echo json_encode(array('success'=>true,'msg'=>__('Guardado con &eacute;xito.'),'Proyecto_id'=>$proyecto_id));
+					exit();
+				}else{
+					echo json_encode(array('success'=>false,'msg'=>__('Su informaci&oacute;n es incorrecta'),'validation'=>$this->Proyecto->validationErrors));
+					exit();
+				}
+			}else{
+				//insert
+				//debug($this->request->data['Proyecto']);exit();
+				if($this->request->data['Proyecto']['imagen']['name'] != ''){
+					$this->request->data['Proyecto']['imagen'] = $this->request->data['Proyecto']['imagen']['name'];
+					
+					//$image_tmp = $this->request->data['Proyecto']['imagen']['tmp_name'];
+					$uploaddir = APP.WEBROOT_DIR.'/files/proyecto/';
+					$uploadfile = $uploaddir . basename($_FILES['data']['name']['Proyecto']['imagen']);
+				
+					move_uploaded_file($_FILES['data']['tmp_name']['Proyecto']['imagen'], $uploadfile);
+				
+				}else{
+					unset($this->request->data['Proyecto']['imagen']);
+				}
+				
+
+				$this->request->data['Proyecto']['estado'] = 1;
+
+				$this->Proyecto->create();
+				if ($this->Proyecto->save($this->request->data)) {
+					$proyecto_id = $this->Proyecto->id;
+					echo json_encode(array('success'=>true,'msg'=>__('La Acci&oacute; fue agregada con &eacute;xito.'),'Proyecto_id'=>$proyecto_id));
+					exit();
+				}else{
+					$proyecto_id = '';
+					echo json_encode(array('success'=>false,'msg'=>__('Su informaci&oacute;n es incorrecta'),'validation'=>$this->Proyecto->validationErrors));
+					exit();
+				}
+			}
+		}else{
+			if(isset($proyecto_id)){
+				$obj_proyecto = $this->Proyecto->findById($proyecto_id);
+				
+				$this->request->data = $obj_proyecto->data;
+				$this->set(compact('proyecto_id','obj_proyecto'));
+			}
+		}
+	}
+	
+	public function delete_proyecto(){
+		$this->layout = 'ajax';
+	
+		$this->loadModel('Proyecto');
+	
+		if($this->request->is('post')){
+			$proyecto_id = $this->request->data['proyecto_id'];
+			
+			$obj_proyecto = $this->Proyecto->findById($proyecto_id);
+			if($obj_proyecto->saveField('estado', 0)){
+				echo json_encode(array('success'=>true,'msg'=>__('Eliminado con &eacute;xito.')));
+				exit();
+			}else{
+				echo json_encode(array('success'=>false,'msg'=>__('Error inesperado.')));
+				exit();
+			}
+			/*if($this->Proyecto->deleteProyecto($proyecto_id)){
+				echo json_encode(array('success'=>true,'msg'=>__('Eliminado con &eacute;xito.')));
+				//exit();
+			}else{
+				echo json_encode(array('success'=>false,'msg'=>__('Error inesperado.')));
+				//exit();
+			}
+			exit();*/
+		}
+	
+	}	
+}
--- app/Model/Proyecto.php
+++ app/Model/Proyecto.php
@@ -2,171 +2,71 @@
 App::uses('AppModel','Model');
   class Proyecto extends AppModel {
     public $name = 'Proyecto';
-    //public $displayField = 'nombre_usuario';
-    
-    //public $virtualFields = array('captcha' => 0);
-    
-    function beforeFilter() {
-        /*$this->Auth->authenticate = ClassRegistry::init('Proyecto');*/
-        parent::beforeFilter();
-    }
-    
-    public $belongsTo = array(
-            'TipoVivienda' => array(
-                    'className' => 'TipoVivienda',
-                    'foreignKey' => 'tipo_vivienda_id',
-                    'conditions' => '',
-                    'fields' => '',
-                    'order' => ''
-            ),
-            'Banco' => array(
-                    'className' => 'Banco',
-                    'foreignKey' => 'bco_id',
-                    'conditions' => '',
-                    'fields' => '',
-                    'order' => ''
-            )
-    );
-    
 
+    
     public $validate = array(
-        'proyectoname'    => array(
-            'required' => array(
-                'rule' => array('notEmpty','isUnique'),
-                'message' => 'Nombre de usuario es requerido'
+            'titulo'    => array(
+                    'notempty' => array(
+                            'rule' => array('notEmpty'),
+                            'message' => 'El titulo es requerida'
+                    )
             )
-        ),
-        'password'     => array(
-            'required' => array(
-                'rule' => array('notEmpty'),
-                'message' => 'La clave es requerida'
-            )
-        )
     );
     
-    public function beforeSave($options = array()) {
     
-        /* password hashing */
-        if (isset($this->data[$this->alias]['password'])) {
-            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-        }
-        return true;
-    }
-    
-    
-    public function listAllProyectos($order_by='Proyecto.created', $search_proyectoname='',$order='DESC') {
-        $arr_obj_proyecto = $this->findObjects('all',array(
-                'conditions'=>array(
-                        'AND' => array(
-                                'Proyecto.proyectoname LIKE'=> '%'.$search_proyectoname.'%',
-                                'Proyecto.estado != ' => 0
-                        )
-                ),
-                'order'=> array($order_by.' '.$order)
-        )
-        );
+  public function listAllProyectos($order_by='Proyecto.created', $search_proyecto='',$order='DESC') {
+            $arr_obj_proyecto = $this->findObjects('all',array(
+                    'conditions'=>array(
+                                    'Proyecto.titulo LIKE'=> '%'.$search_proyecto.'%',
+                                    'Proyecto.estado != ' => 0
+                    ),
+                    'order'=> array($order_by.' '.$order)
+            )
+            );
         return $arr_obj_proyecto;
     }
     
-    public function listFindProyectos($order_by='Proyecto.estado', $search_proyectoname='',$order='DESC', $start=0, $per_page=10) {
-        $arr_obj_proyecto = $this->findObjects('all',array(
-                'conditions'=>array(
-                        'AND' => array(
-                                'Proyecto.proyectoname LIKE'=> '%'.$search_proyectoname.'%',
-                                //'Proyecto.estado != ' => 0
-                        )
-                ),
-                //'page'=> $start,
-                'limit'=> $per_page,
-                'offset'=> $start,
-                'order'=> array('Proyecto.estado DESC'),
-        )
-        );
+    public function listFindProyectos($order_by='Trabajadore.created', $search_proyecto='',$order='DESC') {
+            $arr_obj_proyecto = $this->findObjects('all',array(
+                    'conditions'=>array(
+                            'Proyecto.titulo LIKE'=> '%'.$search_proyecto.'%',
+                            'Proyecto.estado != ' => 0
+                    ),
+                    /*'page'=> $start,
+                    'limit'=> $per_page,
+                    'offset'=> $start,*/
+                    'order'=> array($order_by.' '.$order),
+            )
+            );
         return $arr_obj_proyecto;
     }
     
-    /* Contar los usuarios usado para el Dashboard */
-    public function TotalUsuarios() {
+    /* Usado para ...*/    
+    public function listProyectos() {
         return $this->find('list',
                 array(
-                        /*'fields' => array('id','proyectoname'),*/
+                        'fields' => array('id','proyecto'),
                         'conditions'=>array(
                                 'Proyecto.estado != '=> 0
                         ),
-                        'order' => array('Proyecto.proyectoname ASC')
+                        'order' => array('Proyecto.created ASC')
                 ));
     }
     
     /**
-     * Delete proyectos
-     * @param int $proyecto_id
+     * Delete actividades
+     * @param int $actividad_id
      * @return boolean
      * @author Vladimir
-     * @version 17 Marzo 2015
+     * @version 16 Marzo 2015
      */
     public function deleteProyecto($proyecto_id){
-        if($this->deleteAll(array('Proyecto.id' => $proyecto_id), $cascada = false)){
+    
+        if($this->deleteAll(array('Proyecto.id' => $proyecto_id), $cascada = true)){
             return true;
         }else{
             return false;
         }
-    }
-    
-    public function listarInfoPersonalUsuario(){
-        $arr_obj_info_usuarios = $this->findObjects('all',array(
-                /*'joins' => array(
-                        array(
-                                'table' => 'tipo_usuarios',
-                                'alias' => 'TipoUsuarioJoin',
-                                'type' => 'INNER',
-                                'conditions' => array(
-                                        'TipoUsuarioJoin.id = Usuario.tipo_usuario_id'
-                                )
-                        ),
-                        array(
-                                'table' => 'personas',
-                                'alias' => 'PersonaJoin',
-                                'type' => 'INNER',
-                                'conditions' => array(
-                                        'PersonaJoin.id = Usuario.id'
-                                )
-                        )
-                        array(
-                                'table' => 'personas',
-                                'alias' => 'PersonaJoin',
-                                'type' => 'INNER',
-                                'conditions' => array(
-                                        'PersonaJoin.id = Usuario.id'
-                                )
-                        )
-                        ),*/
-                'order'=> array('Proyecto.created ASC'),
-        )
-        );
-        return $arr_obj_info_usuarios;
-    }
-    
-    
-    public function checkPasswordForProyecto($proyecto_id, $current_pass = null){
-        $obj_proyecto = $this->findById($proyecto_id);
-
-        $old_pass = $obj_proyecto ->getAttr('password');
-        $current_pass_hash = AuthComponent::password($current_pass);
-
-    
-        if($current_pass_hash == $old_pass){
-            return true;
-        }
-        return false;
-    }
-    
-    
-    public function hashPasswords($data) {
-        if (isset($data['Proyecto']['clave'])) {
-            $data['Proyecto']['clave'] = md5($data['Proyecto']['clave']);
-            return $data;
-        }
-        return $data;
     }
     
   }
